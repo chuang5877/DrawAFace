324126cd2e7a755f37b6daf89f7a31d03640ca45
chuang5877 <chuang5877@bths.edu>
2018-11-28 08:38:13 -0500

    Initial commit

---
+++ src/com/company/FaceDrawing.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class FaceDrawing {
+    String[][] faceFeatures;
+    FaceDrawing(String[][] faceFeatures){
+        this.faceFeatures=faceFeatures;
+    }
+    public void edit(String replace,int row,int column){
+
+    }
+    public void fill(String str){
+
+    }
+}
