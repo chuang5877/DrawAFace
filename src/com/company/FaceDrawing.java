d84e311cfbde1c85ed0c28c1d77a6242283c1a3d
chuang5877 <chuang5877@bths.edu>
2018-11-29 08:40:53 -0500

    face

--- src/com/company/FaceDrawing.java
+++ src/com/company/FaceDrawing.java
@@ -2,13 +2,30 @@ package com.company;
 
 public class FaceDrawing {
     String[][] faceFeatures;
-    FaceDrawing(String[][] faceFeatures){
-        this.faceFeatures=faceFeatures;
-    }
-    public void edit(String replace,int row,int column){
 
+    FaceDrawing(String[][] faceFeatures) {
+        this.faceFeatures = faceFeatures;
     }
-    public void fill(String str){
 
+    public void edit(String replace, int row, int column) {
+        faceFeatures[row][column] = replace;
+    }
+
+    public void fill(String str) {
+        for (int i = 0; i < faceFeatures.length; i++) {
+            for (int n = 0; n < faceFeatures[i].length; n++) {
+                    faceFeatures[i][n] = str;
+            }
+        }
+    }
+    public String toString(){
+        String str="";
+        for (int i = 0; i < faceFeatures.length; i++) {
+            for (int n = 0; n < faceFeatures[i].length; n++) {
+                str+=faceFeatures[i][n];
+            }
+            str+='\n';
+        }
+        return str;
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	String[][] face=new String[10][10];
+	FaceDrawing face1=new FaceDrawing(face);
+	face1.fill("O");
+	System.out.println(face1);
     }
 }
