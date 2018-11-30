9e5aeb329b10e633fef64998282fa62b46cf75f6
chuang5877 <chuang5877@bths.edu>
2018-11-29 22:34:06 -0500

    face2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,23 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	String[][] face=new String[10][10];
+	String[][] face=new String[5][8];
 	FaceDrawing face1=new FaceDrawing(face);
-	face1.fill("O");
+	face1.fill("X");
+	face1.edit(" ",0,0);
+	face1.edit(" ",4,7);
+	face1.edit(" ",0,7);
+	face1.edit(" ",4,0);
+	face1.edit("0",1,2);
+	face1.edit("0",1,5);
+	face1.edit("^",2,3);
+	face1.edit("^",2,4);
+	face1.edit("|",3,1);
+	face1.edit("|",3,2);
+	face1.edit("_",3,3);
+	face1.edit("_",3,4);
+	face1.edit("|",3,5);
+	face1.edit("|",3,6);
 	System.out.println(face1);
     }
 }
